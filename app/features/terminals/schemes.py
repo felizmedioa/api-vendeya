14d15d7e942859cd3f6842b6aa39a6dca22fdd56
Luis2709 <luis_jose1978@hotmail.com>
2026-03-02 23:59:09 -0500

    Fix: BaseModel Udefinded

--- app/features/terminals/schemes.py
+++ app/features/terminals/schemes.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 
-class TerminalList(BaseModel):
-    list: list[TerminalFiltered]
 
 class TerminalFiltered(BaseModel):
     id: int
     nombre: str
-    nombre_resumido: str    
+    nombre_resumido: str
+
+
+class TerminalList(BaseModel):
+    list: list[TerminalFiltered]
